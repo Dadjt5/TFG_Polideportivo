2c03635ecc337e8d7f47e4c178d2bc87832acdf7
Dadjt5 <dadjt5@gmail.com>
2026-03-11 18:32:43 +0100

    cambios finales para render

---
+++ backend/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o errexit # exit or error
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+python manage.py makemigrations polideportivo api
+python manage.py migrate
+python manage.py populate
